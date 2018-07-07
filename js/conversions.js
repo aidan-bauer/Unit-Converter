91a2af93d0d893cf692a1c12ffdf09f8f9c9dfd4
aidan-bauer <aidanjbauer94@gmail.com>
2018-07-07 00:28:28 -0400

    Improvements for 7/6

--- js/conversions.js
+++ js/conversions.js
@@ -1,1 +1,17 @@
+const conversions = {
+	inchToCM : 2.5,
+	feetToCM : 30,
+	inchToM : 1 / 250
+};
+
 const inchToCM = 1;
+
+const leftInput = document.getElementById("leftInput");
+const rightInput = document.getElementById("rightInput");
+const imperial = document.getElementById("imperial");
+const metric = document.getElementById("metric");
+
+function determineCoverter(left, right) {
+	const leftPH = left.placeHolder;
+	const rightPH = right.placeHolder;
+}
